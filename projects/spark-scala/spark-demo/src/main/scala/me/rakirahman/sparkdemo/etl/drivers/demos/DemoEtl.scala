126dec944e2df891d35fd6005f41f7ce2fdfd9bd
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-10 17:37:45 +0000

    Use a more modern API, it didn't help

--- projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoEtl.scala
+++ projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoEtl.scala
@@ -20,13 +20,14 @@ object DemoEtl extends App with Logging {
   Array("customers", "orders", "products", "sales").foreach { table =>
     logInfo(s"Creating table: ${table}")
     spark.read
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .csv(s"wasbs://public@rakirahman.blob.core.windows.net/datasets/${table}.csv")
-      .write
-      .format("delta")
-      .mode("overwrite")
-      .saveAsTable(s"${dbName}.${table}")
+         .format("csv")
+         .option("header", "true")
+         .option("inferSchema", "true")
+         .load(s"wasbs://public@rakirahman.blob.core.windows.net/datasets/${table}.csv")
+         .write
+         .format("delta")
+         .mode("overwrite")
+         .saveAsTable(s"${dbName}.${table}")
   }
 
   Seq(
