70783ab98ae045a22d10856591933642ef033c40
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-10 13:06:14 +0000

    Merge ETL a little

--- projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoEtl.scala
+++ projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoEtl.scala
@@ -27,43 +27,47 @@ object DemoEtl extends App {
       .saveAsTable(s"${dbName}.${table}")
   }
 
-  spark
-    .sql(s"""
-    SELECT
-        c.customerID,
-        c.customerName,
-        c.contact,
-        COUNT(o.orderID) as order_count,
-        CASE
-            WHEN COUNT(o.orderID) > 0 THEN TRUE
-            ELSE FALSE
-        END AS has_orders
-    FROM ${dbName}.customers c
-    LEFT JOIN ${dbName}.orders o ON c.customerID = o.customerID
-    GROUP BY c.customerID, c.customerName, c.contact
-    ORDER BY order_count DESC
-  """)
-    .write
-    .format("delta")
-    .mode("overwrite")
-    .saveAsTable(s"${dbName}.customers_cleaned")
-
-  spark
-    .sql(s"""
-    SELECT
-        p.productName,
-        COUNT(s.OrderID) as total_sales,
-        SUM(s.Quantity) as total_quantity,
-        ROUND(AVG(s.Quantity), 2) as avg_quantity_per_sale
-    FROM ${dbName}.products p
-    LEFT JOIN ${dbName}.sales s ON p.productID = s.productID
-    GROUP BY p.productName
-    ORDER BY total_sales DESC
-  """)
-    .write
-    .format("delta")
-    .mode("overwrite")
-    .saveAsTable(s"${dbName}.product_sales_summary")
+  Seq(
+    (
+      "customers_cleaned",
+      s"""
+        SELECT
+            c.customerID,
+            c.customerName,
+            c.contact,
+            COUNT(o.orderID) as order_count,
+            CASE
+                WHEN COUNT(o.orderID) > 0 THEN TRUE
+                ELSE FALSE
+            END AS has_orders
+        FROM ${dbName}.customers c
+        LEFT JOIN ${dbName}.orders o ON c.customerID = o.customerID
+        GROUP BY c.customerID, c.customerName, c.contact
+        ORDER BY order_count DESC
+      """
+    ),
+    (
+      "product_sales_summary",
+      s"""
+        SELECT
+            p.productName,
+            COUNT(s.OrderID) as total_sales,
+            SUM(s.Quantity) as total_quantity,
+            ROUND(AVG(s.Quantity), 2) as avg_quantity_per_sale
+        FROM ${dbName}.products p
+        LEFT JOIN ${dbName}.sales s ON p.productID = s.productID
+        GROUP BY p.productName
+        ORDER BY total_sales DESC
+      """
+    )
+  ).foreach { case (tableName, sqlQuery) =>
+    spark
+      .sql(sqlQuery)
+      .write
+      .format("delta")
+      .mode("overwrite")
+      .saveAsTable(s"${dbName}.${tableName}")
+  }
 
   spark.stopAfter(30.seconds)
 }
