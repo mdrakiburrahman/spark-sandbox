6e75dbb38f7cd2c9d48fea8cc40f42337aab8f63
Raki Rahman <mdrakiburrahman@gmail.com>
2026-01-28 02:02:10 +0000

    Remove unnecessary file

--- projects/spark-dbt/dbt-adventureworks/macros/current_database.sql
+++
@@ -1,8 +0,0 @@
-{# Override the default database macro to return 'lakehouse' instead of null #}
-{% macro default__current_database() %}
-    lakehouse
-{% endmacro %}
-
-{% macro fabricspark__current_database() %}
-    lakehouse
-{% endmacro %}
