d84ba94bf3aac94f875d8f96dbc87dffcb351a50
Raki Rahman <mdrakiburrahman@gmail.com>
2026-02-15 19:10:06 +0000

    Remove database.windows.net

--- projects/spark-scala/.scripts/refresh-az-login.sh
+++ projects/spark-scala/.scripts/refresh-az-login.sh
@@ -25,7 +25,7 @@ echo ">>> Checking generic refresh token"
 az account get-access-token --query "expiresOn" -o tsv >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
     if [[ $IS_GH_ACTION == "1" ]]; then
-        echo "Running in 1ES machine, couldn't retrieve refresh token, trying as ${UAMI_CLIENT_ID}"
+        echo "Running in GitHub machine, couldn't retrieve refresh token, trying as ${UAMI_CLIENT_ID}"
         retry az login --identity --client-id $UAMI_CLIENT_ID
     else
         echo "Running in Visual Studio Code, couldn't retrieve refresh token, trying as end user"
@@ -33,18 +33,4 @@ if [[ $? -ne 0 ]]; then
     fi
 else
     echo ">>> Successfully retrieved existing refresh token."
-fi
-
-echo ">>> Checking refresh token for https://database.windows.net"
-az account get-access-token --resource https://database.windows.net --query "expiresOn" -o tsv >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    if [[ $IS_GH_ACTION == "1" ]]; then
-        echo "Running in 1ES machine, couldn't retrieve refresh token for https://database.windows.net, trying as ${UAMI_CLIENT_ID}"
-        retry az login --identity --client-id $UAMI_CLIENT_ID --scope https://database.windows.net/.default
-    else
-        echo "Couldn't retrieve refresh token for https://database.windows.net, trying as end user - this should open up your browser"
-        az login --scope https://database.windows.net/.default
-    fi
-else
-    echo ">>> Successfully retrieved existing refresh token for https://database.windows.net."
 fi
