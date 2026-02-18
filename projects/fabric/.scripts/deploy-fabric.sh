ec9b3c8012c04923241018c9b05369fd19faba5e
Raki Rahman <mdrakiburrahman@gmail.com>
2026-02-18 16:06:50 +0000

    Add new CICD project

---
+++ projects/fabric/.scripts/deploy-fabric.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#
+#       Wraps fabric-workspace deployment.
+#
+# ---------------------------------------------------------------------------------------
+#
+set -e
+
+args=("$@")
+config_file=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --config-file-absolute-path)
+      config_file="$2"
+      shift 2
+      ;;
+    --operation)
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [[ -n "$config_file" && ! -f "$config_file" ]]; then
+  echo "Config file does not exist - exiting gracefully: $config_file"
+  exit 0
+fi
+
+source "$(realpath $(dirname $0))/common.sh"
+
+export FAB_PATH="$HOME/.local/bin"
+export FAB_TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47"
+export GIT_ROOT=$(git rev-parse --show-toplevel)
+
+export FAB_TOKEN=$(az account get-access-token --resource 'https://analysis.windows.net/powerbi/api' --query accessToken -o tsv)
+export FAB_TOKEN_AZURE=$(az account get-access-token --resource 'https://management.azure.com' --query accessToken -o tsv)
+export FAB_TOKEN_CICD=$(az account get-access-token --resource 'https://api.fabric.microsoft.com' --query accessToken -o tsv)
+
+if [[ $IS_GH_ACTION == "1" ]]; then
+    export LOG_LEVEL="${LOG_LEVEL:-DEBUG}"
+else
+    export LOG_LEVEL="${LOG_LEVEL:-INFO}"
+fi
+
+export FAB_TOKEN_ONELAKE=${FAB_TOKEN}
+
+~/.local/bin/fabric-workspace-deployment "${args[@]}"
---
+++ projects/fabric/.scripts/get-azcopy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Downloads the azcopy utility.
+#
+# See: https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10
+#
+# Downloading this directly allows all nx steps to take a dependency on this before
+# running azcopy commands. If multiple steps try to download it, the OS throws.
+#
+# Usage:
+#   ./get-azcopy.sh dest_dir
+#
+
+set -euo pipefail
+trap '>&2 echo "Error at line $LINENO (process exited with code $?)"' ERR
+
+if [[ $# -ne 1 ]]; then
+    echo >&2 "Invalid or missing argument(s)"
+    exit 1
+fi
+
+dest_dir=$(realpath $1)
+mkdir -p $dest_dir
+
+if [[ -f "$dest_dir/azcopy" ]]; then
+    echo "Found azcopy at: $dest_dir/azcopy"
+    exit 0
+fi
+
+tmp_dir=$(mktemp -d /tmp/azcopyXXXXXX)
+trap 'rm -rf $tmp_dir' EXIT
+
+pushd $tmp_dir >/dev/null
+
+echo "Downloading azcopy"
+wget --no-verbose -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux
+tar -xf azcopy.tar.gz
+azcopy=$(find . -name azcopy -executable -type f | head -n 1)
+
+echo "Installing: $azcopy -> $dest_dir"
+mv $azcopy $dest_dir
+
+popd >/dev/null
