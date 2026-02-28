3c1386c2a34a9b2baf2512df75bfce094ce3814f
Raki Rahman <mdrakiburrahman@gmail.com>
2026-02-28 17:35:12 +0000

    Log tables without lineage

--- projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoLineageExtractor.scala
+++ projects/spark-scala/spark-demo/src/main/scala/me/rakirahman/sparkdemo/etl/drivers/demos/DemoLineageExtractor.scala
@@ -1,6 +1,6 @@
 package me.rakirahman.sparkdemo.etl.drivers.demos
 
-import me.rakirahman.lineage.openlineage.{OpenLineageExtractor, OpenLineageLineage}
+import me.rakirahman.lineage.openlineage.OpenLineageExtractor
 import me.rakirahman.logging.LoggingConstants
 import me.rakirahman.metastore.sql.SqlMetastoreOperations
 import me.rakirahman.spark.SparkSessionManager
@@ -19,23 +19,41 @@ object DemoLineageExtractor extends App with Logging {
   val extractor = OpenLineageExtractor(spark)
   val lineage = extractor.getLineage()
 
+  val tablesWithNoLineage = scala.collection.mutable.ListBuffer.empty[String]
+
   allTables.toSeq.sortBy(_._1).foreach { case (db, tables) =>
     tables.sorted.foreach { table =>
       val fqn = s"$db.$table"
       val filtered = OpenLineageExtractor.filterLineageForDataset(lineage, table)
-      val mermaid = OpenLineageExtractor.toMermaid(filtered, s"Lineage for $fqn")
 
-      val sb = new StringBuilder
-      sb.append(s"${LoggingConstants.mainDivider}")
-      sb.append(s"${LoggingConstants.subDivider}")
-      sb.append(s"Lineage for: $fqn")
-      sb.append(s"${LoggingConstants.subDivider}\n")
-      sb.append(s"Use your browser to paste this into >>> https://www.mermaidflow.app/editor")
-      sb.append(s"${LoggingConstants.subDivider}")
-      sb.append(mermaid)
-      sb.append(s"${LoggingConstants.mainDivider}")
-      logInfo(sb.toString())
+      if (filtered.tableEdges.isEmpty) {
+        tablesWithNoLineage += fqn
+      } else {
+        val mermaid = OpenLineageExtractor.toMermaid(filtered, s"Lineage for $fqn")
+
+        val sb = new StringBuilder
+        sb.append(s"${LoggingConstants.mainDivider}")
+        sb.append(s"${LoggingConstants.subDivider}")
+        sb.append(s"Lineage for: $fqn")
+        sb.append(s"${LoggingConstants.subDivider}\n")
+        sb.append(s"Use your browser to paste this into >>> https://www.mermaidflow.app/editor")
+        sb.append(s"${LoggingConstants.subDivider}")
+        sb.append(mermaid)
+        sb.append(s"${LoggingConstants.mainDivider}")
+        logInfo(sb.toString())
+      }
     }
+  }
+
+  if (tablesWithNoLineage.nonEmpty) {
+    val noLineageSb = new StringBuilder
+    noLineageSb.append(s"${LoggingConstants.mainDivider}")
+    noLineageSb.append(s"${LoggingConstants.subDivider}")
+    noLineageSb.append(s"Tables with no lineage (${tablesWithNoLineage.size}):")
+    noLineageSb.append(s"${LoggingConstants.subDivider}")
+    tablesWithNoLineage.foreach(t => noLineageSb.append(s"  - $t\n"))
+    noLineageSb.append(s"${LoggingConstants.mainDivider}")
+    logInfo(noLineageSb.toString())
   }
 
   val uberMermaid = OpenLineageExtractor.toMermaid(lineage, "UBER Lineage (All Tables)")
